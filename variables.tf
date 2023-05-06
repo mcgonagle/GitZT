3448032900d8ec2578e3ed60712ec64c05324b29
mcgonagle <mcgonagle@gmail.com>
2023-05-06 15:17:04 -0400

    added variables tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+  variable "CF_R2_ACCESS_KEY_ID" {
+    type        = string
+    description = "This is an example input variable using env variables."
+  }
+  variable "CF_R2_SECRET_ACCESS_KEY" {
+    type        = string
+    description = "This is another example input variable using env variables."
+  }
+  variable "CF_ACCOUNT_ID" {
+    type        = string
+    description = "This is another example input variable using env variables."
+  }
